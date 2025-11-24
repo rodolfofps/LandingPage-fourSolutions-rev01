ce3dbbf164e078455c051e3b45f032118120fb2f
Rodolfo Santos <rodolfofps.com>
2025-11-24 15:56:43 -0300

    Criação de component Politica de Privacidade (estrutura)

---
+++ src/components/PrivacyPolicy.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+    return (
+        <div>
+            <p><strong>Termo de Privacidade</strong><br/>
+                Este termo descreve como coletamos, usamos e protegemos suas informações pessoais…
+            </p>
+
+            <p><strong>1. Coleta de informações</strong><br/>
+                Coletamos apenas informações necessárias…
+            </p>
+
+            <p><strong>2. Uso das informações</strong><br/>
+                Usamos seus dados para comunicação…
+            </p>
+
+            <p><strong>3. Direitos do usuário</strong><br/>
+                Você pode solicitar a exclusão ou correção…
+            </p>
+
+            <p>Para mais informações, entre em contato pelo suporte.</p>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
